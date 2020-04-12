bb46d2f6edd50cfe1811486f59f32d207dbaaf2f
AeonX <kedaren.dorasami@gmail.com>
2020-04-12 17:09:28 +0400

    first commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./bootstrap.min.css";
+
+function AuthorQuiz() {
+  return <div>AuthorQuiz</div>;
+}
+
+export default AuthorQuiz;
